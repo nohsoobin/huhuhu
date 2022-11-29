b283048adc19fa4dd932012de5b99970821209ae
nohsoobin <nohsoobin18@naver.com>
2022-11-29 13:30:48 +0900

    aa

--- pages/contact.js
+++ pages/contact.js
@@ -8,6 +8,14 @@ export default function contact() {
       <p>나이: 21세</p>
       <p>사는 곳: 서울시 강서구 화곡6동 ---</p>
       <p>전화번호: 010-5499-7258</p>
+      <div className="basis-1-2">
+        <Image
+          src="/img/me1.jpg"
+          width={300}
+          height={300}
+          layout="responsive"
+        ></Image>
+      </div>
     </div>
   )
 }
